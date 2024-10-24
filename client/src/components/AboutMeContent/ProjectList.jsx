106f43a35a6710378eaa4ab52d20bcb75ca5e42e
Marijana Taseska <marijana.taseska@gmail.com>
2024-10-23 22:24:37 -0700

    build the project data

--- client/src/components/AboutMeContent/ProjectList.jsx
+++ client/src/components/AboutMeContent/ProjectList.jsx
@@ -6,13 +6,29 @@ const projects = [
       link: 'https://github.com/MarijanaTaseska/web-sprint-challenge-authentication-and-testing',
     },
     {
-      name: 'Project 2',
-      link: 'https://github.com/yourusername/project2',
+      name: 'Project 2: Adding Data Persistence Sprint Challenge',
+      link: 'https://github.com/MarijanaTaseska/web-sprint-challenge-adding-data-persistence',
     },
     {
-      name: 'Project 3',
-      link: 'https://github.com/yourusername/project3',
+      name: 'Project 3: Build a Web API',
+      link: 'https://github.com/MarijanaTaseska/web-sprint-challenge-build-a-web-api',
     },
+    {
+        name: 'Project 4: Advanced State Management in React',
+        link: 'https://github.com/MarijanaTaseska/W_S10_Challenge',
+      },
+      {
+        name: 'Project 5: React Router, Forms in React, Form Validation, Testing',
+        link: 'https://github.com/MarijanaTaseska/web-sprint-challenge-advanced-react',
+      },
+      {
+        name: 'Project 6: Advanced React Sprint Challenge',
+        link: 'https://github.com/MarijanaTaseska/web-s7-challenge-MarijanaTaseska',
+      },
+      {
+        name: 'Project 7: Manipulating the DOM and building a section using vanilla JavaScript',
+        link: 'https://github.com/MarijanaTaseska/sprint-5-challenge-MarijanaTaseska',
+      },
     // Add more projects here
   ];
 
