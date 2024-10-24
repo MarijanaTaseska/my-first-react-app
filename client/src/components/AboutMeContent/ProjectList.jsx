a1f36d6bceaea0acbefc564f39bf6232c22ab43a
Marijana Taseska <marijana.taseska@gmail.com>
2024-10-23 22:12:43 -0700

    added another component

---
+++ client/src/components/AboutMeContent/ProjectList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const projects = [
+    {
+      name: 'Project 1: Authentication and Testing Sprint Challenge',
+      link: 'https://github.com/MarijanaTaseska/web-sprint-challenge-authentication-and-testing',
+    },
+    {
+      name: 'Project 2',
+      link: 'https://github.com/yourusername/project2',
+    },
+    {
+      name: 'Project 3',
+      link: 'https://github.com/yourusername/project3',
+    },
+    // Add more projects here
+  ];
+
+const ProjectList = () => {
+    return (
+        <div>
+            <h2>My Projects</h2>
+        </div>
+    )
+}
+
+export default ProjectList;
